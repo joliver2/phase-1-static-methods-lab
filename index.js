1aeb62745f94ddb561413b06ae4371b5c7a6b207
joliver427@icloud.com <joliver427@icloud.com>
2023-01-18 16:22:53 -0700

    commit tree 6da5501cec7d115607d2f9aa68ac096e6d939254

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "");
+  }
+
+  static titleize(string) {
+    const words = string.split(" ")
+    words[0] = words[0].charAt(0).toUpperCase() + words[0].slice(1)
+    for (let i = 1; i < words.length; i++) {
+      if (words[i] !== "a" && words[i] !== "an" && words[i] !== "but" && words[i] !== "of" && words[i] !== "and" && words[i] !== "for" && words[i] !== "at" && words[i] !== "by" && words[i] !== "from" && words[i] !== "the") {
+       words [i] = words[i].charAt(0).toUpperCase() + words[i].substr(1);
+      }
+    } const string2 = words.join(" ")
+      return string2;
+  } 
 }
